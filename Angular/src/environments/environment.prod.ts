c374bd85c3adecbe42dc4cce5931263dd4a7a7bf
Benjamin Ivan <ivan1516hc@gmail.com>
2024-04-10 09:42:54 -0600

    Dist

--- Angular/src/environments/environment.prod.ts
+++ Angular/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl:"https://tramites.difzapopan.gob.mx/laravel_jwt/public/api",
-  dowload: "https://tramites.difzapopan.gob.mx/laravel_jwt/public/documents/"
+  baseUrl:"https://tramites.comude.gob.mx/laravel_jwt/public/api",
+  dowload: "https://tramites.comude.gob.mx/laravel_jwt/public/documents/"
 };
